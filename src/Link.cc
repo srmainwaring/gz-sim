63d978f304b1528b56c22983a990929fb211c603
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-06-16 12:20:16 +0100

    Link: mark EntityWrenchMap as OneTimeChange
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- src/Link.cc
+++ src/Link.cc
@@ -594,7 +594,7 @@ void Link::AddWorldWrench(EntityComponentManager &_ecm,
     (*data.mutable_wrenches())[label] = msg;
 
     _ecm.SetChanged(this->dataPtr->id, components::EntityWrenchMap::typeId,
-        ComponentState::PeriodicChange);
+        ComponentState::OneTimeChange);
 
     // {
     //   gzdbg << "Publishing entity wrench map for link ["
